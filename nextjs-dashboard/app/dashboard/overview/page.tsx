69ba3461a8892dd4e946cebc29f31b58eda846f6
BraianPerez97 <BraianPerez97@proton.me>
2024-06-29 11:59:09 -0400

    feat: Update dashboard page with lazy loading for images

--- nextjs-dashboard/app/dashboard/overview/page.tsx
+++ nextjs-dashboard/app/dashboard/overview/page.tsx
@@ -1,10 +1,12 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchRevenue, fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+import {fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+
 
 
 export default async function Page() {
@@ -15,6 +17,7 @@ export default async function Page() {
         totalPendingInvoices} = await fetchCardData();
     
 return (
+
 <main>
     <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
     Dashboard
@@ -31,9 +34,9 @@ return (
     </div>
     <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-        <RevenueChartSkeleton /> 
+            <RevenueChart />
         </Suspense>
-    { <LatestInvoices latestInvoices={latestInvoices} /> }
+        <LatestInvoices latestInvoices={latestInvoices} />
     </div>
 </main>
 );
